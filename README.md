bd2456feff93e2a0fa1839b629c7db2beefa2720
Raisel Melian <raisel.melian@viacom.com>
2020-07-31 19:28:44 -0500

    update readme

--- README.md
+++ README.md
@@ -1,9 +1,35 @@
-# Index dynamodb data to elasticsearch
-This repo shows how to index dynamodb data to elasticsearch. 
+# Index DynamoDB data to elasticsearch
 
-![Diagram](assets/diagram.png)
+### What is this?
+A serverless application that sets up a lambda function to consume data from a DynamoDB Stream and save it to an Elasticsearch cluster
 
-* A lambda function is reponsible for getting the records from DynamoDB Streams and coping or updating Elasticsearch. 
+### The Architecture
+* A lambda function is responsible for consuming records from DynamoDB Streams and replicating it into Elasticsearch. 
 * Python is used as lambda function language
 * Serverless framework is used to build and deploy the services to AWS
 
+![Diagram](assets/diagram.png)
+
+
+### Tech Stack
+* [Serverless Framework](https://www.serverless.com/)
+* [Amazon DynamoDB](https://aws.amazon.com/dynamodb/)
+* [AWS Lambda](https://aws.amazon.com/lambda/)
+* [Amazon Elasticsearch](https://aws.amazon.com/elasticsearch-service/)
+
+
+### Prerequisite
+* [Serverless framework](https://www.serverless.com/framework/docs/providers/aws/guide/installation/)
+* [Python](https://wiki.python.org/moin/BeginnersGuide/Download)
+
+
+### How to run it
+```
+npm install
+```
+```
+sls deploy
+```
+
+### Author: Raisel Melian
+* [Twitter](https://twitter.com/raiselmelian)
--- metadata-indexer.py
+++ metadata-indexer.py
@@ -4,20 +4,20 @@ from requests_aws4auth import AWS4Auth
 
 es_host = "https://search-mini-ma-assete-i9pybhqdcgv0-q2qqdrsdhqy6ewqrnnsu6wlp5i.us-east-1.es.amazonaws.com"
 es_index = "metadata"
-es_type = "videos"
+es_type = "episodes"
 url = es_host + '/' + es_index + '/' + es_type + '/'
 
-region = 'us-east-1'
+region = 'us-east-2'
 service = 'es'
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
 def handler(event, context):
     print(event)
-    # for record in event['Records']:
-    #     id = str(record['dynamodb']['Keys']['pk']['S'])
-    #     if record['eventName'] == 'REMOVE':
-    #         res = requests.delete(url + id, auth=awsauth)
-    #     else:
-    #         document = record['dynamodb']['NewImage']
-    #         res = requests.put(url + id, auth=awsauth, json=document, headers={"Content-Type": "application/json"})
+    for record in event['Records']:
+        id = str(record['dynamodb']['Keys']['id']['S'])
+        if record['eventName'] == 'REMOVE':
+            res = requests.delete(url + id, auth=awsauth)
+        else:
+            document = record['dynamodb']['NewImage']
+            res = requests.put(url + id, auth=awsauth, json=document, headers={"Content-Type": "application/json"})
